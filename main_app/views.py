5a2c8d6303561fd09906b4acc97279a08e125022
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-11-28 16:11:42 +0300

    feeding

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields= '__all__'
+    success_url='/finches/'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  success_url='/finches/'
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
+
+
+
 
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
-
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
 
 
 def finches_index(request):
