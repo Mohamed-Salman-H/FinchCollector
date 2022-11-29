7af79b84f9095441ecc0eb0fa616c5bdbeaa2948
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-11-29 15:17:05 +0300

    auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_color = models.CharField(max_length=50)
 
 
 MEALS = (
@@ -7,14 +15,34 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 
 
 class Feeding(models.Model):
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Finch
+from .models import Finch, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class FinchCreate(CreateView):
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+
+class FinchCreate(LoginRequiredMixin, CreateView):
     model = Finch
     fields= '__all__'
     success_url='/finches/'
+
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+      form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+      return super().form_valid(form)
 
 def add_feeding(request, finch_id):
   # create a ModelForm instance using the data in request.POST
@@ -21,6 +53,7 @@ def add_feeding(request, finch_id):
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)
+
 
 class FinchUpdate(UpdateView):
   model = Finch
@@ -38,12 +71,13 @@ class FinchDelete(DeleteView):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
     feeding_form = FeedingForm()
-    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 
-
+@login_required
 def finches_index(request):
-    finches_list = Finch.objects.all()
+    finches_list = Finch.objects.filter(user=request.user)
     return render(request, 'finches/index.html', {
         'finches': finches_list
     })
@@ -54,3 +88,25 @@ def about(request):
 
 def home(request):
     return render(request, 'home.html')
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
